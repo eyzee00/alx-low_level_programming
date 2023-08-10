6d25677f19a3e76dd61920c6fbb80c1061fd8eda
Hamza <azizihamza1910@gmail.com>
2023-08-10 14:31:27 -0700

    Added a C program that prints a string to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point, prints a string to standard error
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+return (1);
+}

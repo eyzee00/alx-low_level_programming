21825731ff5e179e5d390176ee03dad6b15be476
Hamza <azizihamza1910@gmail.com>
2023-08-10 14:44:05 -0700

    !

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+char  error[61] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(error, 61, 1, stderr);
 return (1);
 }

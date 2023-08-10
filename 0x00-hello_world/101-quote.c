c93b4e5f6b3ac010cd8c09c20d1f31ec9d5d64b6
Hamza <azizihamza1910@gmail.com>
2023-08-10 15:02:52 -0700

    !

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 /**
  * main - Entry point, prints a string to standard error
  *
@@ -6,7 +7,6 @@
  */
 int main(void)
 {
-char  error[61] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fwrite(error, 61, 1, stderr);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }

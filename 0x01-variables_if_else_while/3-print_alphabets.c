d78fb4f1cbc0f367036ffcee2e9fcc7574ff4030
Hamza <azizihamza1910@gmail.com>
2023-08-11 12:33:20 -0700

    !

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - prints alphabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 97;
+
+	while (x < 123)
+	{
+		putchar(x);
+		x++;
+	}
+	x = 65;
+	while (x < 91)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return 0;
+}

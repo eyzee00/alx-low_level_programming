0caca490d80b1aff71a54429b3f14b595a3b82a2
Hamza <azizihamza1910@gmail.com>
2023-08-11 12:40:14 -0700

    !

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point, prints alphabet in lowercase except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 97;
+
+	while (x > 123)
+	{
+		if (x != 'q' || x != 'e')
+			putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}

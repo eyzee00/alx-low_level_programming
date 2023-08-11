e07dbf81c3b715a7382c18891fbde0b634375c2d
Hamza <azizihamza1910@gmail.com>
2023-08-11 15:00:06 -0700

    !

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+/** main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 48, y;
+
+	while (x < 58)
+	{
+		y = x + 1;
+		while (y < 58)
+		{
+			putchar(x);
+			putchar(y);
+			if (x < 56 || y < 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}

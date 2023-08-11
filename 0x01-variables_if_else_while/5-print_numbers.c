6444d1b39ed5be23c73a12d6d04645abc998cfce
Hamza <azizihamza1910@gmail.com>
2023-08-11 12:49:43 -0700

    !

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	short x = 0;
+
+	while (x < 10)
+	{
+		prinft("%d", x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}

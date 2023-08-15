d302481e18adac2721a882b926d1c4c5ed3b8d89
eyzee00 <azizihamza1910@gmail.com>
2023-08-15 22:13:17 +0100

    !

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,18 @@
 
 int print_last_digit(int x)
 {
+	int last_digit, last_digit_abs;
+	
+	last_digit = x % 10;
+	last_digit_abs = last_digit * (-1);
 	if (x >= 0)
 	{
-		_putchar((x % 10) + '0');
-		return (x % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
 	}
 	else
 	{
-		int a = x * (-1);
-
-		_putchar((a % 10) + '0');
-		return (a % 10);
+		_putchar(last_digit_abs + '0');
+		return (last_digit_abs);
 	}
 }

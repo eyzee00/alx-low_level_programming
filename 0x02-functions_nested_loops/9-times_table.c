df94b512e7b0ab000f4789305490e24849daa8b3
eyzee00 <azizihamza1910@gmail.com>
2023-08-15 22:58:27 +0100

    !

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,22 +8,30 @@
 
 void times_table(void)
 {
-	int i = 0, j = 0;
-
+	int i = 0, j = 0, prod;
+	
 	while (i < 10)
 	{
 		while (j < 10)
 		{
-			_putchar((i * j) + 48);
+			prod = i * j;
+			if (prod / 10 == 0)
+			{
+				_putchar(prod + 48);
+			}
+			else
+			{
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
 			if (j < 9)
 			{
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(',');
 			}
 			j++;
 		}
-		j = 0
+		j = 0;
 		_putchar('\n');
 		i++;
 	}

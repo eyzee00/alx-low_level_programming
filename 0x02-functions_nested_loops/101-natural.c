5c906d4048fd60582c9086ffef7d3ec3d2608169
eyzee00 <azizihamza1910@gmail.com>
2023-08-16 03:18:48 +0100

    !

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /** 
  * main - Entry point
  * Description: prints the sum of numbers that are multiples
@@ -11,6 +10,7 @@
 int main(void)
 {
 	int i, sum = 0;
+
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)

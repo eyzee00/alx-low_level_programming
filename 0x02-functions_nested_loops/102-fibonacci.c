9f0952b3f6ac51f9589a205c6ecac592a2860267
eyzee00 <azizihamza1910@gmail.com>
2023-08-16 03:35:34 +0100

    !

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: prints the first 50 fibonacci terms
+ * Return: (0)
+ */
+int main(void)
+{
+	int i = 0;
+	long x = 1, y = 2;
+	for ( ; i <= 50; i++)
+	{
+		if (i == 0)
+		{
+			printf("%ld", x);
+		}
+		else if (i == 1)
+		{
+			printf(", %ld", y);
+		}
+		else
+		{
+			y = y + x;
+			x = y - x;
+			printf(", %ld", y);
+		}
+	}
+	printf("\n");
+	return (0);
+}

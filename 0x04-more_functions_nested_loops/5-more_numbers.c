e4b369a697f1e57f0143a14ea4f531c8cbc7f75f
eyzee00 <azizihamza1910@gmail.com>
2023-08-17 16:39:24 +0100

    !

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if ((i / 10) != 0)
+			if ((j / 10) != 0)
 			{
-				_putchar((i / 10) + 48);
+				_putchar((j / 10) + 48);
 			}
 
-			_putchar((i % 10) + 48);
+			_putchar((j % 10) + 48);
 		}
 
 		_putchar('\n');

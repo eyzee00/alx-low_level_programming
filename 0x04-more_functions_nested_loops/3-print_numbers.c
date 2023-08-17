ae661960ea3568953591f9dec6fe64a9ea3507e4
eyzee00 <azizihamza1910@gmail.com>
2023-08-17 16:07:20 +0100

    !

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,17 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9
- * 
  * Return: void
  */
 
 void print_numbers(void)
 {
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + 48);
 	}
-	
+
 	_putchar('\n');
 }

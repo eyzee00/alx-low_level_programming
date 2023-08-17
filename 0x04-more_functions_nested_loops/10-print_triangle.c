40d2a27034041b4a400a7030421087df83523fac
eyzee00 <azizihamza1910@gmail.com>
2023-08-17 19:08:58 +0100

    !

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle: prints a triangle using '#'
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int row, i, j;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (i = 1; i <= size - row; i++)
+		{
+			_putchar(32);
+		}
+		for (j = 1; j <= row; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}

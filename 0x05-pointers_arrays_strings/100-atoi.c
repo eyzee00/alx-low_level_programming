a2ff672b4b41f1978b1704d84d0d3cbb1d317a63
eyzee00 <azizihamza1910@gmail.com>
2023-08-21 22:25:27 +0100

    !

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <string.h>
+#include <stdbool.h>
+/**
+ * _atoi - converts a string to integer
+ * @s: string to be converted
+ * Return: the equivalent integer to s
+ */
+
+int _atoi(char *s)
+{
+	bool isnegative = false, hasnumbers = false;
+	int i, integer = 0;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == '-')
+		{
+			if (!isnegative)
+				isnegative = true;
+			else
+				isnegative = false;
+		}
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			hasnumbers = true;
+			integer = integer * 10 + (s[i] - 48);
+			i++;
+		}
+		if (hasnumbers)
+			break;
+	}
+	if (!hasnumbers)
+		return (0);
+
+	else
+	{
+		if (isnegative)
+			return (integer * (-1));
+		else
+			return (integer);
+	}
+}

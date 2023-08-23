8f8da1df0b76de4b1613fe2ed73e13564544a442
eyzee00 <azizihamza1910@gmail.com>
2023-08-23 22:56:06 +0100

    !

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,16 +13,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i, s1_len = (int)strlen(s1), s2_len = (int)strlen(s2);
-	int sum = 0;
+	int diff = 0;
 
 	for (i = 0; i < s1_len || i < s2_len; i++)
 	{
-		if (i >= s1_len)
-			sum = s2[i] * (-1);
-		if (i >= s2_len)
-			sum = s1[i];
+		if (s1[i] == s2[i])
+			continue;
 		else
-			sum = s1[i] - s2[i];
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
 	}
-	return (sum);
+	return (diff);
 }

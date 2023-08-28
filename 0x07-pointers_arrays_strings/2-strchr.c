673c5cd83fbcb098a1c01e3d52b2914b16e9330d
eyzee00 <azizihamza1910@gmail.com>
2023-08-28 19:40:20 +0100

    !

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * _strchr - locates character c in _strchr
+ * @s: the string operated on
+ * @c: the character we are looking for
+ * Return: Address of the first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i, bool isfound = false;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if(*(s + i) == c)
+		{
+			isfound = true;
+			break;
+		}
+	}
+	if (!isfound)
+		return (NULL);
+	else
+		return (s + i);
+}

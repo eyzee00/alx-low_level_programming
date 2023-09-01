4dce96c62dc92e7dbe5619c2f1182130cc4633a9
eyzee <azizihamza1910@gmail.com>
2023-09-01 18:21:41 +0100

    !

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point, takes change as argument
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: minimum number of coins amounting to change
+ * (1) if more or less than 1 argument is included
+ */
+int main(int argc, char **argv)
+{
+	int coin_num, tmp_amount, i;
+	int coins[5] = {1, 2, 5, 10, 25};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		coin_num = 0;
+		tmp_amount = atoi(argv[1]);
+		while (tmp_amount > coins[4])
+		{
+			tmp_amount = tmp_amount - coins[4];
+			coin_num++;
+		}
+		while (tmp_amount < coins[4] && tmp_amount > coins[3])
+		{
+			tmp_amount = tmp_amount - coins[3];
+			coin_num++;
+		}
+		while (tmp_amount < coins[3] && tmp_amount > coins[2])
+		{
+			tmp_amount = tmp_amount - coins[2];
+			coin_num++;
+		}
+		while (tmp_amount < coins[2] && tmp_amount > coins[1])
+		{
+			tmp_amount = tmp_amount - coins[1];
+			coin_num++;
+		}
+		for (i = 0; i < 5; i++)
+		{
+			if (tmp_amount == coins[i])
+			{
+				coin_num++;
+				break;
+			}
+		}
+		printf("%d\n", coin_num);
+		return (0);
+	}
+}

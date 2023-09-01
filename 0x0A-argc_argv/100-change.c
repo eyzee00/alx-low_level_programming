a99a4fb7a622d7f2cfbb8480db2d75ec79c95a54
eyzee <azizihamza1910@gmail.com>
2023-09-01 18:51:12 +0100

    !

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,42 +6,30 @@
  * @argv: argument vector
  * Return: minimum number of coins amounting to change
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	int coin_num = 0, tmp_amount = atoi(argv[1]), i, coins[5] = {1, 2, 5, 10, 25};
+	int coin_num, cent_amount;
+	int coin_set[5] = {1, 2, 5, 10, 25};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (tmp_amount > coins[4])
+	cent_amount = atoi(argv[1]);
+	for (coin_num = 0; cent_amount > 0; coin_num++)
 	{
-		while(tmp_amount >= coins[4])
-			tmp_amount = tmp_amount - coins[4], coin_num++;
-	}
-	if (tmp_amount < coins[4] && tmp_amount > coins[3])
-	{
-		while(tmp_amount >= coins[3])
-			tmp_amount = tmp_amount - coins[3], coin_num++;
-	}
-	if (tmp_amount < coins[3] && tmp_amount > coins[2])
-	{
-		while(tmp_amount >= coins[2])
-			tmp_amount = tmp_amount - coins[2], coin_num++;
-	}
-	if (tmp_amount < coins[2] && tmp_amount > coins[1])
-	{
-		while (tmp_amount >= coins[1])
-			tmp_amount = tmp_amount - coins[1], coin_num++;
-	}
-	for (i = 0; i < 5; i++)
-	{
-		if (tmp_amount == coins[i])
-		{
-			coin_num++;
-			break;
-		}
+		if (cent_amount - coin_set[4] >= 0)
+			cent_amount = cent_amount - coin_set[4];
+		else if (cent_amount - coin_set[3] >= 0)
+			cent_amount = cent_amount - coin_set[3];
+		else if (cent_amount - coin_set[2] >= 0)
+			cent_amount = cent_amount - coin_set[2];
+		else if (cent_amount - coin_set[1] >= 0)
+			cent_amount = cent_amount - coin_set[1];
+		else if (cent_amount - coin_set[0] >= 0)
+			cent_amount = cent_amount - coin_set[0];
 	}
 	printf("%d\n", coin_num);
 	return (0);

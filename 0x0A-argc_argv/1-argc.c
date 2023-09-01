b1004c5ef7912f7acfaba2d89fab57601e7e7754
eyzee <azizihamza1910@gmail.com>
2023-09-01 16:55:53 +0100

    !

---
+++ 0x0A-argc_argv/1-argc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry Point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

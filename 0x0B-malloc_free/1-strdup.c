0d5e2acb4560340984370d270e0d674952719abd
eyzee <azizihamza1910@gmail.com>
2023-09-05 01:22:24 +0100

    !

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,6 +15,8 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+
+	size = (int)sizeof(str);
 	str_copy = malloc((size + 1) * sizeof(char));
 	if (str_copy == NULL)
 		return (NULL);

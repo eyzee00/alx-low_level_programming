4c1b392350dc4ce6925e5c76bebef12ed35d38b9
eyzee <azizihamza1910@gmail.com>
2023-09-07 19:18:29 +0100

    !

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,13 @@
 	void *_calloc(unsigned int nmemb, unsigned int size);
 	int *array_range(int min, int max);
 	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+	int is_digit(char *string);
+	void init_string(char *string, unsigned int length);
+	int _strlen(char *string);
+	void reverse(char *string, int length);
+	void _puts(char *string);
+	char *zero_purge(char *string, int length);
+	char *zero_scanner(char *string, int length);
+	char *infinite_mul(char *str1, char *str2, int str1_len, int str2_len);
 
 #endif

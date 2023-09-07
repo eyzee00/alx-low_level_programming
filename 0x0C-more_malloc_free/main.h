236dc47c485c606f4bd41b16ab07f47d056a1878
eyzee <azizihamza1910@gmail.com>
2023-09-07 21:49:34 +0100

    !

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,9 +10,10 @@
 	void init_string(char *string, unsigned int length);
 	int _strlen(char *string);
 	void reverse(char *string, int length);
-	void print_string(char *string);
+	void printstr(char *string);
 	char *zero_purge(char *string, int length);
 	char *zero_scanner(char *string, int length);
+	void _putchar(char c);
 	char *infinite_mul(char *str1, char *str2, int str1_len, int str2_len);
 
 #endif

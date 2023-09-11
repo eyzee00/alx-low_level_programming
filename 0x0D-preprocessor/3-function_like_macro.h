4351f87f621acc7f632bebe624388274030d3d88
eyzee <azizihamza1910@gmail.com>
2023-09-11 19:37:01 +0100

    !

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef __3__function__like__macro__h__
-#define __3__function__like__macro__h__
-#define ABS(x) ((x <= 0) ? ((-1) * x) : x)
+#ifndef __3__FUNCTION__LIKE__MACRO__H__
+#define __3__FUNCTION__LIKE__MACRO__H__
+#define ABS(x) (x <= 0 ? X * (-1) : x)
 #endif

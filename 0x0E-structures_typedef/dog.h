fc80d7a6a3a3e05d9184c053ff584d9270e954c4
eyzee <azizihamza1910@gmail.com>
2023-09-11 20:50:15 +0100

    !

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef __DOG__H__
+#define __DOG__H__
+/**
+ * struct dog - new structure named dog
+ * @name: name of the dog
+ * @owner: the owner of the dog
+ * @age: the age of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

526e7798a1c453d92969ee60fa550863e066d504
eyzee <azizihamza1910@gmail.com>
2023-09-13 19:38:14 +0100

    !

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
  * print_name - prints a string, using the given function as arg
- * @f(): the function to used to print (name)
+ * @f: the function to used to print (name)
  * @name: the string to be printed
  * Return: void
  */

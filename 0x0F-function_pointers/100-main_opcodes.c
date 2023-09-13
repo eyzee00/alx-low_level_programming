633a63d5e830cfa2a429da5b423a5483fdfd16f3
eyzee <azizihamza1910@gmail.com>
2023-09-13 23:57:14 +0100

    !

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the opcodes of the main function
+ * in hexadecimal lowercase, each opcode is 2 char long
+ * Return: (0) if successful
+ */
+int main(int argc, char **argv)
+{
+	int byte_count, i;
+	char *opcode_string;
+
+	if (argc < 2 || argc > 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (argv[1] < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	opcode_string = (char *)main;
+	byte_count = atoi(*(argv + 1));
+	i = 0;
+	while (i < byte_count)
+	{
+		if (i != byte_count - 1)
+			printf("%02hhx ", *(opcode_string + i));
+		else
+			printf("%02hhx\n", *(opcode_string + i));
+		i++;
+	}
+	return (0);
+}

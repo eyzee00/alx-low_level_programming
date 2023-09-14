7a9c58342dd9c7cf4d90f99d17d03e26f5e0b31e
eyzee <azizihamza1910@gmail.com>
2023-09-14 22:25:28 +0100

    !

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_char - prints the character pointed to by arg_list
+ * @arg_list: pointer to the argument to print
+ * Return: (void)
+ */
+void print_char(va_list arg_list)
+{
+	printf("%c", va_arg(arg_list, int));
+}
+/**
+ * print_int - prints the int pointed to by arg_list
+ * @arg_list: pointer to the int
+ * Return: void
+ */
+void print_int(va_list arg_list)
+{
+	printf("%d", va_arg(arg_list, int));
+}
+/**
+ * print_float - prints the float pointed to by arg_list
+ * @arg_list: pointer to the float
+ * Return: (void)
+ */
+void print_float(va_list arg_list)
+{
+	printf("%f", va_arg(arg_list, double));
+}
+/**
+ * print_string - prints the string pointed to by arg_list
+ * @arg_list: pointer to the string
+ * Return: (void)
+ */
+void print_string(va_list arg_list)
+{
+	char *string = va_arg(arg_list, char *);
+
+	if (string == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", string);
+}
+/**
+ * print_all - makes use of all the above functions
+ * @format: a list of all the directives, any other character
+ * besides it woll be ignored
+ * Return: (void)
+ */
+void print_all(const char *const format, ...)
+{
+	unsigned int i = 0, j = 0;
+	direc directive_list[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+	char *separator = "";
+	va_list arg_ptr;
+
+	va_start(arg_ptr, format);
+	while (*(format + i) != 0 && format != 0)
+	{
+		while (j < 4)
+		{
+			if (*directive_list[j].type == format[i])
+			{
+				printf("%s", separator);
+				directive_list[j].fun(arg_ptr);
+				separator = ", ";
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	printf("\n");
+	va_end(arg_ptr);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,25 @@
 #ifndef __VARIADIC__FUNCTION__
 #define __VARIADIC__FUNCTION__
+#include <stdarg.h>
+/**
+ * struct directive - structure consisting of type and a pointer
+ * to a function to execute
+ * @type: the type of the format specifier
+ * @fun: the function associated with the type
+ */
+typedef struct directive
+{
+	char* type;
+	void (*fun)(va_list);
+} direc;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list arg_list);
+void print_int(va_list arg_list);
+void print_float(va_list arg_list);
+void print_string(va_list arg_list);
 
 #endif

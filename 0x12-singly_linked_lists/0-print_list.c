91966d2f8d13c09b396da2441503d136f26636f7
eyzee <azizihamza1910@gmail.com>
2023-09-21 20:28:31 +0100

    Added a function that prints the elemets of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * print_list - prints list elements starting from the head
+ * @head: the head of the list (the first node)
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+	list_t *iter = (list_t *) h;
+
+	if (iter == NULL)
+		return (0);
+	while (iter != NULL)
+	{
+		++size;
+		if (iter->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", iter->len, iter->str);
+		iter = iter->next;
+	}
+	return (size);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef __LISTS__H__
+#define __LISTS__H__
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: a pointer to the next node (or the rest of the list)
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/*Singly linked lists functions*/
+size_t print_list(const list_t *h);
+
+#endif

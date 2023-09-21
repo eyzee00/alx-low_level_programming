92b502a38b5e97b52a81c2317c26b1c53dcbd0a4
eyzee <azizihamza1910@gmail.com>
2023-09-22 00:00:05 +0100

    Added a function that prints a string before main starts executing

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_before_main - prints a string before main
+ * Return: (void)
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}

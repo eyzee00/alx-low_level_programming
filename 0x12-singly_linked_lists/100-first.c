73eba4323b505741612d375ace4efa1a3b4f595d
eyzee <azizihamza1910@gmail.com>
2023-09-22 00:08:55 +0100

    fixed wrong output case

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,8 @@
  * print_before_main - prints a string before main
  * Return: (void)
  */
-void __attribute__((constructor)) print_before_main()
+void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }

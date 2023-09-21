eb05c8e7308f60c4fa69d578522cd0a32a048523
eyzee <azizihamza1910@gmail.com>
2023-09-22 00:05:58 +0100

    Trying to fix a failing check

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include "lists.h"
+#include <sys/cdefs.h>
 /**
  * print_before_main - prints a string before main
  * Return: (void)
  */
-void print_before_main(void)
+void __attribute__((constructor)) print_before_main()
 {
 	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back!\n");

4401ce82cd3ce414756bfd095b3c149f6df1357a
eyzee <azizihamza1910@gmail.com>
2023-09-22 00:41:52 +0100

    !

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,13 +5,13 @@ section .data 	         ;defining printf args
 extern printf            ;declare printf as external function
 global main              ;declare main as global function
 
-section .text            ;code sectiom
+section .text            ;code section
 main:
 	push rbp         ;push rbp pointer onto the stack
 	mov rdi, format  ;load the address of format in rdi (arg 1)
 	mov rsi, message ;load the address of message in rsi (arg 2)
-	mov rax, 0       ;clear rax register
+	xor rax, rax       ;clear rax register
 	call printf      ;call printf with the above arguments
 	pop rbp          ;pop rbp pointer from the stack
-	mov rax, 0       ;set rax to 0 (to indicate success)
+	xor rax, rax       ;set rax to 0 (to indicate success)
 	ret              ;return

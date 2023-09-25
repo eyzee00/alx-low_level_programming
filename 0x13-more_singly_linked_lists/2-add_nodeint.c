1774eefacce70a305636c78041cd2523abccd891
eyzee <azizihamza1910@gmail.com>
2023-09-25 21:44:08 +0100

    Handled the case when memory allocation fails

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,6 +10,8 @@ listint_t *add_nodeint(listint_t **h, const int n)
 {
 	listint_t *newnode = malloc(sizeof(listint_t));
 
+	if (newnode == NULL)
+		return (NULL);
 	newnode->n = n;
 	if (*h == NULL)
 	{

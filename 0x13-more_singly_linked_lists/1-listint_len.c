7fd0d043b0f01a6ea86e55fc08ea83028fb79410
eyzee <azizihamza1910@gmail.com>
2023-09-25 21:23:40 +0100

    Added a function that counts the number of nodes in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - calculates the number of nodes in a linked lists
+ * @h: a pointer to the first node (the head)
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	listint_t *iter = h;
+	size_t nodecount = 0;
+
+	while (iter != NULL)
+	{
+		nodecount++;
+		iter = iter->next;
+	}
+	return (nodecount);
+}

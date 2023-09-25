a1f1fa5db3ef3e5d6aa072deea0e072c3612715c
eyzee <azizihamza1910@gmail.com>
2023-09-25 22:01:34 +0100

    Added a function that prints memory previously allocated for a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the memory spaces allocated previously for the given list
+ * @h: a pointer to the head of the list (the first node)
+ * Return: (void)
+ */
+void free_listint(listint_t *h)
+{
+	listint_t *placeholder;
+
+	if (h == NULL)
+		return;
+	while (h != NULL)
+	{
+		placeholder = h;
+		h = h->next;
+		free(placeholder);
+	}
+}

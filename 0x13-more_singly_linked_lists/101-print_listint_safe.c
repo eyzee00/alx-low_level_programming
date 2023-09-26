48ff56fd46eba89c10392527f5c8ff924999860b
eyzee <azizihamza1910@gmail.com>
2023-09-26 21:38:07 +0100

    Added a function that prints a singly linked list safely

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+
+/**
+ * free_addrlist - frees an address list
+ * @head: the address of the pointer to the first node (the head)
+ * Return: (void)
+ */
+void free_addrlist(addrlist_t *head)
+{
+	addrlist_t *placeholder, *iter = head;
+
+	while (iter != NULL)
+	{
+		placeholder = iter;
+		iter = iter->next;
+		free(placeholder);
+	}
+}
+/**
+ * add_addr - adds an address to the previously visited memory regions
+ * @head: the address of the pointer to the first node
+ * @address: the address to add
+ * Return: (void)
+ */
+void add_addr(addrlist_t **head, const listint_t *address)
+{
+	addrlist_t *newnode = malloc(sizeof(listint_t));
+
+	if (newnode == NULL)
+	{	
+		free_addrlist(*head);
+		exit(98);
+	}
+	newnode->address = (void *)address;
+	newnode->next = *head;
+	*head = newnode;	
+}
+/**
+ * print_listint_safe - prints a list while checking for infinite
+ * loops cause by faulty pointers
+ * @h: a pointer to the first node (the head)
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	addrlist_t *curraddr = NULL, *iter;
+	size_t nodecount = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		iter = curraddr;
+		while (iter != NULL)
+		{
+			if (iter->address == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_addrlist(curraddr);
+				exit(98);
+			}
+			iter = iter->next;
+		}
+		nodecount++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		add_addr(&curraddr, head);
+		head = head->next;
+	}
+	free_addrlist(curraddr);
+	return (nodecount);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdint.h>
 /**
  * struct listint_s - a linked list struct containing an integer as data
  * @n: an integer
@@ -13,6 +14,16 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+/**
+ * struct addrlist_s - a linked list containing an address as data
+ * @addr: a void address
+ * @next: a pointer to the next element in the list (or the rest of the list)
+ */
+typedef struct addrlist_s
+{
+	void *address;
+	struct addrlist_s *next;
+} addrlist_t;
 
 /*general purpose functions*/
 size_t print_listint(const listint_t *h);
@@ -27,5 +38,8 @@ int sum_listint(listint_t *h);
 listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+void add_addr(addrlist_t **head, const listint_t *address);
+void free_addrlist(addrlist_t *head);
 
 #endif

ee1d7cbf42ff11e1b5bb17a4dd606f75542779d9
eyzee <azizihamza1910@gmail.com>
2023-09-28 20:38:33 +0100

    !

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,24 @@
 #include "main.h"
 /**
+ * power - calculates the first arg to the power of the second
+ * @n: integer
+ * @expo: exponent
+ * Return: the result of the operation
+ */
+int power(int n, int expo)
+{
+	int i = 0, res = 1;
+
+	if (expo == 0)
+		return (1);
+	while (i < expo)
+	{
+		res = res * n;
+		i++;
+	}
+	return (res);
+}
+/**
  * binary_to_uint - converst a biary number to decimal
  * @b: the binary number represented as string
  * Return: the number in decimal, (0) if the string contains
@@ -7,16 +26,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, expo = 0, decimal = 0;
+	int i = 0, expo = 0, decimal = 0;
 
 	if (b == NULL)
 		return (0);
-	for (i = 0; i < strlen(b); i++)
+	for (i = 0; i < (int)strlen(b); i++)
 		;
 	for (i = i - 1; i >= 0; i--, expo++)
 	{
+		if (b[i] != 48 && b[i] != 49)
+			return (0);
 		if (b[i] == 49)
-			decimal += pow(2, expo);
+			decimal += power(2, expo);
 	}
 	return (decimal);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
 #ifndef __MAIN__H__
 #define __MAIN__H__
-#include <math.h>
 #include <string.h>
+
 unsigned int binary_to_uint(const char *b);
+int power(int n, int expo);
 
 #endif

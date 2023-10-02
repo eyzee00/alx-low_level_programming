6de38f9fb244c3f3b556aaec39ad6828c94be4b1
eyzee <azizihamza1910@gmail.com>
2023-10-02 22:48:43 +0100

    !

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	filedesc = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	filedesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (filedesc < 0)
 		return (-1);
 	if (text_content != NULL)

479905e747a4c4f806ae7800211dd6cad845af95
eyzee <azizihamza1910@gmail.com>
2023-10-02 22:40:41 +0100

    Added a fucntion that creates a new file and writes given text in it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_file - creates a file and sets its permissions to rw-------
+ * @filename: the name of the file
+ * @text_content: the content to be wrtitten in the file
+ * Return: (1) on success, otherwise (-1)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int writec = 0, filedesc = 0;
+
+	if (filename == NULL)
+		return (-1);
+	filedesc = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	if (filedesc < 0)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+	writec = write(filedesc, text_content, strlen(text_content));
+	if (writec < 0)
+		return (-1);
+	if (close(filedesc) < 0)
+		return (-1);
+	return (writec);
+}

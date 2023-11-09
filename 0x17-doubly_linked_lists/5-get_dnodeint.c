a7fc5de92b1ebdf78ea2ac18aa7862541d61a8ac
eyzee <azizihamza1910@gmail.com>
2023-11-09 20:59:09 +0100

    Removed an unnecessary conditional statement

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *iter = head;
 	unsigned int i = 0;
 
-	if (index < 0 || head == NULL)
+	if (head == NULL)
 		return (NULL);
 	while (iter != NULL && i < index)
 	{

fa16fe2ca16238c8aa9b0b5339f029f4c4112cbb
eyzee <azizihamza1910@gmail.com>
2023-11-09 21:33:31 +0100

    !

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (iter == NULL)
 			return (NULL);
 	}
+	if (iter->next == NULL)
+		return (add_dnodeint_end(h, n));
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 		return (NULL);

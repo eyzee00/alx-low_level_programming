ba6d8af772d3176390154cf25f751c1f77d20db3
eyzee <azizihamza1910@gmail.com>
2023-11-09 20:28:52 +0100

    Added a link between the head of the list and the newnode

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,19 +2,22 @@
 /**
  * add_dnodeint - adds a node a the beginning of the linked list
  * @head: the address of the pointer to the first element
- * Return: the address of the created element, NULL if allocation fails
+ * @n: the value of the new element
+ * Return: the address of the created element,
+ * NULL if allocation fails
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode = NULL;
 
-	newnode = (dlistint_t *) malloc(sizeof(dlistint_t));
+	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
+	if (*head != NULL)
+		(*head)->prev = newnode;
 	newnode->prev = NULL;
 	newnode->next = *head;
-	(*head)->prev = newnode;
 	*head = newnode;
 	return (newnode);
 }

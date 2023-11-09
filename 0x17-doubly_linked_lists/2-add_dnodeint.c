ee1a7de1a947ee93135cc29b26798d11cb89ab72
eyzee <azizihamza1910@gmail.com>
2023-11-09 20:21:59 +0100

    Added a link between the head of the list and the newnode

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newnode->n = n;
 	newnode->prev = NULL;
 	newnode->next = *head;
+	(*head)->prev = newnode;
 	*head = newnode;
 	return (newnode);
 }

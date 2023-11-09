381bd08fcfb3b09a5c40dede76298b9066b16a7d
eyzee <azizihamza1910@gmail.com>
2023-11-09 19:59:47 +0100

    Added a function that prints all the elements in a doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * print_dlistint - prints all the elements of a doubly
+ * linked list
+ * @head: points to the first element of the list
+ * Return: length of the list
+ */
+size_t print_dlistint(const dlistint_t *head)
+{
+	dlistint_t *iter = (dlistint_t *) head;
+	size_t count = 0;
+
+	if (iter == NULL)
+		return (0);
+	while (iter->next != NULL)
+	{
+		printf("%d\n", iter->n);
+		count++;
+	}
+	return (count);
+}

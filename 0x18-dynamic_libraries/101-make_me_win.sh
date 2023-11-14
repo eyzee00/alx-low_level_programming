ccd371b8d2385ff22532aa25dd9f6ad8b8981bdb
eyzee <azizihamza1910@gmail.com>
2023-11-14 01:00:37 +0100

    !

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/eyzee00/alx-low_level_programming/blob/master/0x18-dynamic_libraries/hack.so
+wget -P /tmp https://github.com/eyzee00/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hack.so
 export LD_PRELOAD=/tmp/hack.so

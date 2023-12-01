64ee533c6626d5631de169c07f15918e1085224f
eyzee00 <azizihamza1910@gmail.com>
2023-12-01 22:46:33 +0100

    !

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: pointer to the hash table
+ * Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *iterator = NULL;
+	unsigned long int i = 0, j = 0;
+	char *value = NULL;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		iterator = ht->array[i];
+		while (iterator != NULL)
+		{
+			value = iterator->value;
+			printf("'%s': '%s'", iterator->key, value);
+			iterator = iterator->next;
+			if (iterator != NULL)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}

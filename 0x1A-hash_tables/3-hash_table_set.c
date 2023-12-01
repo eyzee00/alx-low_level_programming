c7851adc062e13db09e4252c2b1f86b03d4ef98d
eyzee00 <azizihamza1910@gmail.com>
2023-12-01 22:38:29 +0100

    !

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	i = index;
 	while (ht->array[i] != 0)
 	{

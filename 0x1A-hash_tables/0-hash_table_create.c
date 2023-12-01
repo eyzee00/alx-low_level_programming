86611253a78b96ca79e37b3361e2d1d404224a3a
eyzee00 <azizihamza1910@gmail.com>
2023-12-01 22:09:49 +0100

    !

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  * Return: if an error occurs - NULL
  * Otherwise - a pointer to the new hash table
  */
-hash_node_t *hash_table_create(unsigned long int size)
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable = NULL;
 	unsigned long int i = 0;
